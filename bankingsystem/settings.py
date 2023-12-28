22b11d001fca1e73229cbed047d024771c253c09
Mycosign <154969560+Mycosign@users.noreply.github.com>
2023-12-28 18:04:05 +0100

    Update settings.py

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'simpleui',
+    'admin_soft.apps.AdminSoftDashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
